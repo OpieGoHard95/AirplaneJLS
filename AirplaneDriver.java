6fca63da44643d7f05ff8a1f3ec9eb66c5a6f5e0
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-13 15:59:57 -0500

    Needed to fix, bug in old repo

---
+++ AirplaneDriver.java
@@ -0,0 +1,10 @@
+// Coded by Jared Smith
+// Driver test file for the Airplane class
+// Started on 2/11/15
+
+public class AirplaneDriver {
+	public static void main(String[] args) {
+	  // Now create an airplane
+	  Airplane myplane = new Airplane("Make","Model",1999,100);
+	}
+	}	
